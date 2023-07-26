c8db23b79a808579b443fa3c7a968cd28bae70f4
mkloz <micha21cloz@gmail.com>
2023-07-26 20:41:56 +0100

    seeder speed fix

--- prisma/seeds/admin-create.seed.ts
+++ prisma/seeds/admin-create.seed.ts
@@ -1,33 +1,22 @@
-import { PrismaClient, Provider } from '@prisma/client';
+import { Provider } from '@prisma/client';
 import * as dotenv from 'dotenv';
-import { CartService } from '@cart/cart.service';
 import { Done } from '@shared/dto';
-import { PrismaService } from '@db/prisma.service';
-import { UserRepository } from '@user/user.repository';
 import { UserService } from '@user/user.service';
-import { ArticleService } from '@article/article.service';
 import { ApiConfigService } from '@config/api-config.service';
-import { ConfigService } from '@nestjs/config';
-import { NestApplication, NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from '@app/app.module';
 
 dotenv.config();
 
-export async function createAdmin(prisma: PrismaClient): Promise<Done> {
-  const admin = {
-    name: process.env.ADMIN_NAME || '',
-    email: process.env.ADMIN_EMAIL || '',
-    password: process.env.ADMIN_PASSWORD || '',
-  };
+export async function createAdmin(
+  userService: UserService,
+  acs: ApiConfigService,
+): Promise<Done> {
+  const admin = acs.getOnlineStore().admin;
+
   for (const key in Object.keys(admin)) {
     if (!key) throw new Error('Provide env variables!');
   }
   admin.password = await UserService.hashPassword(admin.password);
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app
-    .get(UserService)
-    .createAdmin({ ...admin, provider: Provider.EMAIL });
+  await userService.createAdmin({ ...admin, provider: Provider.EMAIL });
 
   return new Done();
 }
